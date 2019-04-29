dc037474e8a53b24475eb803c325c6a878c35311
Won Kim <wonsae.kim@gmail.com>
2019-04-29 16:41:52 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    result = []
+    array.each do |word|
+      if word.chars.sort == self.word.chars.sort
+        result << word
+      end
+    end
+    result
+  end
+end
