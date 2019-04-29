94e06d51bdf8ab56c24a6fa6a1e33221b2705ca9
Jason Gomez <jason.gomez@me.com>
2019-04-29 17:49:45 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,15 +7,7 @@ class Anagram
   end
 
   def match(array)
-    ans = []
-    array.each do |word|
-      letter = []
-      word.each_char { |c| letter << c }
-      s_letter = []
-      string.each_char { |c| s_letter << c }
-      ans << word if letter.sort == s_letter.sort#letter.all? { |l| string.include?(l) } && word.length == string.length && letter.sort == s_letter.sort
-      #ans << word if letter == s_letter
-    end
-    ans
+    array.select { |word| word.split("").sort == string.split("").sort}
   end
+
 end
