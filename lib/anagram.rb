d2b8835359277728dd0eb71f762cf0348a3ea646
Jason Gomez <jason.gomez@me.com>
2019-04-29 17:41:30 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(array)
+    ans = []
+    array.each do |word|
+      letter = []
+      word.each_char { |c| letter << c }
+      s_letter = []
+      string.each_char { |c| s_letter << c }
+      ans << word if letter.sort == s_letter.sort#letter.all? { |l| string.include?(l) } && word.length == string.length && letter.sort == s_letter.sort
+      #ans << word if letter == s_letter
+    end
+    ans
+  end
+end
