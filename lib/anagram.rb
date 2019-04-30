af60163837185a6f423c7667cdc90be5c60be79e
Qunli <qunli02@gmail.com>
2019-04-30 10:58:50 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arg)
+    letter = self.word.split("").sort
+    arg.select do |word|
+      word.split("").sort == letter
+    end
+  end
+
+end
